36df5807de2217271e04b6d03c9d959310cfe75f
Robert Forkel <xrotwang@googlemail.com>
2017-02-02 10:45:56 +0100

    refactored cli, for usage see README.md (#17)
    * refactored cli, for usage see README.md
    
    * make profile command honour the --mapping option, too.
    
    * enhance programmatic usability
    
    * enhance programmatic usability

--- segments/__init__.py
+++ segments/__init__.py
@@ -1,2 +1,3 @@
-#
+from segments.tokenizer import Tokenizer, Profile, Rules
+
 __version__ = '1.0.0'
--- segments/tests/test_cli.py
+++ segments/tests/test_cli.py
@@ -1,7 +1,6 @@
 # coding: utf8
 from __future__ import unicode_literals, print_function, division
 from unittest import TestCase
-import os
 import sys
 from contextlib import contextmanager
 
@@ -25,23 +24,14 @@ class Tests(TestCase):
     def test_tokenize(self):
         from segments.cli import tokenize
 
-        with capture(tokenize, Mock(args=['abc'.encode('utf8')], encoding='utf8')) as out:
+        with capture(
+            tokenize,
+            Mock(args=['abc'.encode('utf8')], encoding='utf8', profile=None)
+        ) as out:
             self.assertEqual(out.strip(), 'a b c')
 
     def test_profile(self):
         from segments.cli import profile
-
-        stream = StringIO('abcd')
-        with capture(profile, Mock(args=[], encoding='utf8'), stream=stream) as o:
-            stream.close()
-            self.assertIn('a\t1\ta', o.split('\n'))
-
-        with capture(
-            profile,
-            Mock(args=[os.path.join(os.path.dirname(__file__), 'test.prf')],
-                 encoding='utf8')
-        ) as o:
-            self.assertTrue(bool(o.split('\n')))
 
         with capture(profile, Mock(args=['abcaba'.encode('utf8')], encoding='utf8')) as o:
             self.assertIn('a\t3\ta', o.split('\n'))
