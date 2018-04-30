9c193beacda756746def9903d142fe976f3b5168
xrotwang <xrotwang@googlemail.com>
2018-04-30 12:51:07 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='segments',
-    version="1.2.1",
+    version="1.2.2.dev0",
     description='',
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
