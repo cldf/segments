120418cd3a120fa2d6b190bfa6eed4efcb86d171
xrotwang <xrotwang@googlemail.com>
2018-08-01 22:46:16 +0200

    release 2.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='segments',
-    version="1.2.3.dev0",
+    version="2.0.0",
     description='',
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
