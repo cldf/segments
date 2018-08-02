9136a4ec89555bf9b574399ffbb07f3cc9a9f45f
xrotwang <xrotwang@googlemail.com>
2018-08-02 10:55:33 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='segments',
-    version="2.0.1",
+    version="2.0.2.dev0",
     description='',
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
--- src/segments/__init__.py
+++ src/segments/__init__.py
@@ -2,5 +2,5 @@ from segments.tokenizer import Tokenizer, Rules  # noqa: F401
 from segments.profile import Profile  # noqa: F401
 from segments.util import REPLACEMENT_MARKER  # noqa: F401
 
-__version__ = '2.0.1'
+__version__ = '2.0.2.dev0'
 __all__ = ['Tokenizer', 'Profile', 'Rules']
